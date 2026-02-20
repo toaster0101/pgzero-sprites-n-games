9a76e081f8788f886d769fb4f239800811211c3d
toaster0101 <henry.bray.star@gmail.com>
2026-02-20 16:59:48 +0000

    Add files via upload

---
+++ game1.py
@@ -0,0 +1,33 @@
+import pgzrun
+import random
+WIDTH=400
+HEIGHT=400
+plane=Actor("plem")
+yn=""
+count=0
+flag=1
+hits=0
+def draw():
+    screen.fill("red")
+    plane.draw()
+    screen.draw.text(yn,topright=(380,0),color=("white"))
+    if flag==0:
+        plane.x=500
+        plane.y=500
+        screen.draw.text("End of game, You got:\n|"+str(hits)+"",topright=(380,0),color=("white"))
+def on_mouse_down(pos):
+    global yn,count,flag,hits
+    if count!=21:
+        count+=1
+        if plane.collidepoint(pos):
+            print("yarrrr")
+            plane.x=random.randint(0,400)
+            plane.y=random.randint(0,400)
+            yn="yarrrr"
+            hits+=1
+        else:
+            print("narrrr")
+            yn="narrrr"
+    else:
+        flag=0
+pgzrun.go()
